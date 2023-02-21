af1b37dbff7eaa39483ba3e5b21cca45ed6a8700
traxcer <meloy30@hotmail.com>
2023-02-21 20:19:32 +0100

    trabajo

--- tema_2/practica_1/p2/GuardaDatos.c
+++ tema_2/practica_1/p2/GuardaDatos.c
@@ -5,14 +5,27 @@ Manuel Eloy Gutiérrez
 
 #include <stdio.h>
 
+struct Estructura{
+    int a;
+    float b;
+    char c;
+};
+
 
 
 int man(int argc, char *argv[]){
-
+//Control argumentos
     if (argc<2){
         printf("Usar: %s <nombfichero",argv[0]);
         exit (1);
     }
+
+//Datos
+struct Estructura es;
+es.a = 1; es.b = 2.0; es.c = '3';
+int x = 7;
+int array_enteros[4]={0x00, 0x01, 0x02, 0x03};
+
 
 
 
