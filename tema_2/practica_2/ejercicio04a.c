4bacc1ccf25393e363475db35d007f8e134c8a38
traxcer <meloy30@hotmail.com>
2023-03-08 13:29:33 +0100

    portatil HP

---
+++ tema_2/practica_2/ejercicio04a.c
@@ -0,0 +1,52 @@
+#define _POSIX_CODE
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+
+/* Ejercicio 4.a
+
+a) Escribe un programa que, periódicamente, (3,3 segundos de CPU) 
+imprima un “.” en la pantalla. Protégelo contra Ctrl-C. 
+
+(NOTA: para pararlo, usa Ctrl-Z y luego mátalo por línea de comandos)
+*/
+
+void m (int signo);
+
+void m (int signo){
+    signal(SIGALRM,m);
+    write(1,".",1);
+}
+
+
+int main(){
+    
+    /*Protejo contra CTRL+C*/
+    signal(SIGINT,SIG_IGN);
+    
+    struct itimerval temporizo; /* asigno valores*/
+    temporizo.it_interval.tv_sec=3;
+    temporizo.it_interval.tv_usec=300000;
+    temporizo.it_value.tv_sec=3;
+    temporizo.it_value.tv_usec=300000;
+    
+    /* Ajusto el Timer*/
+    if((setitimer(ITIMER_REAL, &temporizo,NULL))<0){
+        perror("signal");
+        exit(1);
+    }
+    
+    if((signal (SIGALRM,m))<0){
+        perror("signal");
+        exit(1);
+    }
+    
+    while (1)
+        pause();
+
+return 0;
+}
