fc6b1b3f212a1376ba21e5d78d7b3cd6a15e6f8a
Traxcer <meloy30@hotmail.com>
2023-03-26 15:52:58 +0200

    imac actualizarcion

--- tema_2/practica_3b/ejercicio02.c
+++ tema_2/practica_3b/ejercicio02.c
@@ -14,10 +14,20 @@ finalizar.
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 #define T 50
 
-int main(){
+/* Prototipos */
+int read_n(int fd,void *b, int n);
+
+/* Función principal */
+int main(int argc, char *argv[]){
+if (argc<2){
+    printf("Usar: %s <int a leer>\n", argv[0]);
+    exit(1);
+}
+
 /* Preparo las pipe para canalizar la comunicación (ph)PADRE -> HIJO 
     y la comunicación inversa (hp)HIJO -> PADRE */
     int ph[2], hp[2]; /**/
@@ -35,49 +45,107 @@ int main(){
         perror("fork");
         exit(1);
     } else if (pid==0){ /* Proceso HIJO*/
-    if((close (p[1]))<0){
-        perror("Close Escritura Hijo");
+    int t_leidos=0;
+    if((close (ph[1]))<0){
+        perror("Close Escritura Padre->Hijo");
+        exit(1);
+    }
+    if((close (hp[0]))<0){
+        perror("Close Lectura Hijo->Padre");
         exit(1);
     }
     char mensaje[T];
+    char texto[T];
     int leidos;
-    leidos=read(p[0],mensaje,T);
+    leidos=read(ph[0],mensaje,T);
     do{
-    //write (1,"Mensaje leido (HIJO):\n",23);
-    write(1,mensaje,leidos);
-    } while ((leidos=read(p[0],mensaje,T))>0);
+        //write (1,"Mensaje leido (HIJO):\n",23);
+        t_leidos+=leidos;
+        sprintf(texto,"(HIJO)%s (t_leido)%d", mensaje,t_leidos);
+        write(1,texto,strlen(texto));
+        if (t_leidos <= atoi(argv[1]))
+            write(hp[1],"y",1);
+        else
+            write(hp[1],"n",1);
+
+    } while ((leidos=read(ph[0],mensaje,T))>0);
     if (leidos<0){
         perror("leidos");
         exit(1);
     }
+    if((close (hp[1]))<0){
+        perror("Close Escritura Hijo->Padre");
+        exit(1);
+    }
+    if((close (ph[0]))<0){
+        perror("Close Lectura Padre->Hijo");
+        exit(1);
+    }
+
     exit(1); /* Termina el hijo */
     
     } else { /* Proceso PADRE */
-    if((close (p[0]))<0){
-        perror("Close Lectura Padre");
+    if((close (ph[0]))<0){
+        perror("Close Lectura Padre->Hijo");
+        exit(1);
+    }
+    if((close (hp[1]))<0){
+        perror("Close Escritura Hijo->Padre");
         exit(1);
     }
     char mensaje[T];
+    char msg[T];
     int teclado;
+    char msgctr='y';
         do{
             //write (1,"Introduce un Mensaje (PADRE):\n",31);
             if((teclado=read(0,&mensaje,T))<0){
                 perror("read teclado");
                 exit(1);
             }
-        /* Escribe en tuberia el MSG*/
-        if ((write(p[1],mensaje,teclado))<0){
-            perror("write del padre en pipe");
+            mensaje[teclado]='\0'; /*convierte en string*/
+            /* Escribe en tuberia el MSG*/
+            if ((write(ph[1],mensaje,teclado-1))<0){
+                perror("write del padre escribiendo P->H");
+                exit(1);
+            }
+            if (read_n(hp[0],&msgctr,1)<0){
+                perror("read_n");
+                exit(1);
+            }
+            sprintf(msg,"(PADRE)%s (ctrl)%c\n",mensaje,msgctr);
+            write(1,msg,strlen(msg));
+        } while (msgctr=='y');
+        if((close (ph[1]))<0){
+            perror("(PADRE) Close Escritura Padre->Hijo");
             exit(1);
-        }
-        mensaje[teclado-1]='\0';
-        } while (strcmp(mensaje,"fin")!=0);
-
-        if((close (p[1]))<0){
-            perror("Close Escritura del Padre");
+        }   
+        if((close (hp[0]))<0){
+            perror("(PADRE) Close Lectura Hijo->Padre");
             exit(1);
         }   
     wait(NULL); /* Espera al hijo */
     }
+return 0; /* Fin del main() */
+}
+
+
+/* Funciones Auxiliares */
+int read_n(int fd,void *b, int n){
+    int a_leer=n;
+    int t_leido=0;
+    int leido;
+    do
+    {
+        errno=0;
+        leido=read(fd,b+t_leido,a_leer);
+        if (leido >=0){
+            a_leer-=leido;
+            t_leido+=leido;
+        }
+    } while ( ((leido>0)&&(t_leido<n))|| errno==EINTR);
+    if (t_leido > 0)
+        return t_leido;
+    return leido;
 
 }
